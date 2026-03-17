ca3b5f3d29b5eabe641c7096f8184669a42c1593
Sam Deane <sam@elegantchaos.com>
2026-03-17 14:36:37 +0000

    Moved button into a separate file

--- Sources/CommandsUI/ImporterCommandButton.swift
+++ Sources/CommandsUI/ImporterCommandButton.swift
@@ -7,17 +7,23 @@ import Commands
 import SwiftUI
 import UniformTypeIdentifiers
 
+public enum ImporterCommandURLState {
+  case unknown
+  case chosen([URL])
+  case error(Error)
+}
+
 public protocol ImporterCommand: CommandWithUI {
   var types: [UTType] { get }
   var allowsMultipleSelection: Bool { get }
-  mutating func setURLS(_ urls: [URL])
+  var state: ImporterCommandURLState { get set }
 }
 
 /// Button wrapper that presents an importer sheet before invoking a command.
 struct ImporterCommandButton<C: ImporterCommand, CC: CommandCentre>: View where C.Centre == CC {
   @State var isShowingSheet = false
+  @State var command: C
 
-  let command: C
   let centre: CC
   let role: ButtonRole?
 
@@ -39,12 +45,11 @@ struct ImporterCommandButton<C: ImporterCommand, CC: CommandCentre>: View where 
       ) { result in
         switch result {
         case .success(let urls):
-          var c = command
-          c.setURLS(urls)
-          centre.performWithoutWaiting(c)
-        case .failure:
-          break  // TODO: handle error
+          command.state = .chosen(urls)
+        case .failure(let error):
+          command.state = .error(error)
         }
+        centre.performWithoutWaiting(command)
       }
     }
   }
